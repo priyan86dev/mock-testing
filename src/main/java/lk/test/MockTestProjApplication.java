be2b246dcc4115406c637746beea9bd96210a2e7
priyan86dev <priyan86dev@gmail.com>
2021-05-04 15:10:55 +0530

    initial commit.

---
+++ src/main/java/lk/test/MockTestProjApplication.java
@@ -0,0 +1,13 @@
+package lk.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTestProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/test/MockTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
